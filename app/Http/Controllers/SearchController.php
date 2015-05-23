397dd526cff6f2e6a3ee6fffa5f3f20f80c73417
Denis Ramirez <deensky@gmail.com>
2015-05-22 19:55:38 -0700

    Add search query validation.

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,12 +2,13 @@
 
 use Quincalla\Product;
 use Quincalla\Http\Requests;
+use Quincalla\Http\Requests\SearchQueryRequest;
 use Quincalla\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Quincalla\Request;
 
 class SearchController extends Controller {
 
-	public function index()
+	public function index(SearchQueryRequest $request)
 	{
         $query = \Request::get('query');
         $results = Product::search($query)->get();
