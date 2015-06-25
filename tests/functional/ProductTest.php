d89353d3851e0707c95085112e146f2f17431b74
Denis Ramirez <deensky@gmail.com>
2015-06-24 22:32:53 -0700

    Add Products integration test.

---
+++ tests/functional/ProductTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Quincalla\Tests\Functional;
+
+use Quincalla\Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ProductTest extends TestCase
+{
+
+    public function testProductTitle()
+    {
+        $this->visit('/products/first-necklace-yellow-gold')
+            ->see('First Necklace Yellow Gold');
+    }
+}
+
