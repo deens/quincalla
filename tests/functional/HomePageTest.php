91887656995e301855983bbbdfa0223075fac7e4
Denis Ramirez <deensky@gmail.com>
2015-06-24 22:05:43 -0700

    Add Home page test.

---
+++ tests/functional/HomePageTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Quincalla\Tests\Functional;
+
+use Quincalla\Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class HomePageTest extends TestCase
+{
+
+    public function testHomePage()
+    {
+        $this->visit('/')
+            ->see('quincalla');
+    }
+}
