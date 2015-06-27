3f42995c726458273b5a08ae4a9ebef9b7cd9f40
Eliurkis <eliurkis@gmail.com>
2015-06-27 15:54:49 -0400

    Add countries packages & debugbar config

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder {
 		$this->call('UserTableSeeder');
 		$this->call('CollectionTableSeeder');
 		$this->call('ProductTableSeeder');
+        $this->call('CountriesSeeder');
 	}
 
 }
