e71cde27cb2d18b2273ff2c79d8bc0b58ce1fbb7
Denis Ramirez <deensky@gmail.com>
2015-06-27 11:56:56 -0700

    Add tags migrations and model.

--- app/Product.php
+++ app/Product.php
@@ -24,4 +24,9 @@ class Product extends Model {
     {
         return $this->belongsTo('Quincalla\Collection');
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany('Quincalla\Tag');
+    }
 }
