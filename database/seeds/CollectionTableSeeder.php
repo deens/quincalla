a9e62596a0c7dbce2d63b6c025190e878a3d0d2d
Denis Ramirez <deensky@gmail.com>
2015-07-20 18:46:19 -0700

    Add a couple of smart collections.

--- database/seeds/CollectionTableSeeder.php
+++ database/seeds/CollectionTableSeeder.php
@@ -3,20 +3,48 @@
 use Illuminate\Database\Seeder;
 use Quincalla\Entities\Collection;
 
-class CollectionTableSeeder extends Seeder {
-
+class CollectionTableSeeder extends Seeder
+{
     public function run()
     {
         DB::table('collections')->delete();
-        Collection::create([
-            'name' => 'Frontpage',
-            'slug' => 'frontpage',
-            'type' => 'manual',
-            'published' => false,
-            'created_at' => new DateTime(),
-            'updated_at' => new DateTime()
-        ]);
+        Collection::insert($this->collections());
     }
 
+    public function collections()
+    {
+        return [
+            [
+                'name' => 'Frontpage',
+                'slug' => 'frontpage',
+                'type' => 'manual',
+                'match' => 'all',
+                'sort_order' => 'name-asc',
+                'rules' => '',
+                'published' => false,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ], [
+                'name' => 'Apple',
+                'slug' => 'apple',
+                'type' => 'condition',
+                'match' => 'all',
+                'sort_order' => 'name-asc',
+                'rules' => '[[{"column":"vendor"},{"relation":"is_equal_to"},{"condition":"Apple"}]]',
+                'published' => true,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ], [
+                'name' => 'Nike',
+                'slug' => 'nike',
+                'type' => 'condition',
+                'match' => 'all',
+                'sort_order' => 'name-asc',
+                'rules' => '[[{"column":"vendor"},{"relation":"is_equal_to"},{"condition":"Nike"}]]',
+                'published' => true,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ]
+        ];
+    }
 }
-
