8903edf1087eeb0eecaed39c8ade34ae07f376c2
Denis Ramirez <deensky@gmail.com>
2015-07-22 18:01:53 -0700

    Assign tags to products.

--- app/Entities/Tag.php
+++ app/Entities/Tag.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     protected $table = 'tags';
-    protected $fillable = [];
+    protected $fillable = ['name'];
 
     public function products()
     {
--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -54,8 +54,11 @@ class ProductsController extends Controller
      */
     public function store(Request $request)
     {
+        $tagSync = $this->prepareTagsIds($request->input('tags'));
+
         $product = $this->products->create($request->all());
-        $product->tags()->attach($request->input('tags'));
+
+        $product->tags()->sync($tagSync);
 
         return redirect()
             ->route('admin.products.index')
@@ -87,10 +90,12 @@ class ProductsController extends Controller
      */
     public function update($id, Request $request)
     {
-        dd($request->all());
         $product = $this->products->findOrFail($id);
+
+        $tagSync = $this->prepareTagsIds($request->input('tags'));
+
         $product->update($request->all());
-        $product->tags()->sync($request->input('tags'));
+        $product->tags()->sync($tagSync);
 
         return redirect()
             ->route('admin.products.index')
@@ -107,5 +112,27 @@ class ProductsController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    /**
+     * Return a ids from old and newly created tags.
+     *
+     * @param array $tags List of tags ids and new tags
+     *
+     * @return array
+     */
+    private function prepareTagsIds($tags)
+    {
+        $tagIds = [];
+        foreach ($tags as $key => $tag) {
+            if (!is_numeric($tag)) {
+                $newTag = $this->tags->firstOrCreate(['name' => $tag]);
+                $tagIds[] = $newTag->id;
+            } else {
+                $tagIds[] = $tag;
+            }
+        }
+
+        return $tagIds;
     }
 }
