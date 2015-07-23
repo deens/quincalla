4801473ba0341a85776b0f40d2c913178087ce75
Denis Ramirez <deensky@gmail.com>
2015-07-22 20:49:57 -0700

    Move sync product tags to Tag entity.

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -54,11 +54,8 @@ class ProductsController extends Controller
      */
     public function store(Request $request)
     {
-        $tagSync = $this->prepareTagsIds($request->input('tags'));
-
         $product = $this->products->create($request->all());
-
-        $product->tags()->sync($tagSync);
+        $this->tags->syncProductTags($product, $request->input('tags'));
 
         return redirect()
             ->route('admin.products.index')
@@ -91,11 +88,9 @@ class ProductsController extends Controller
     public function update($id, Request $request)
     {
         $product = $this->products->findOrFail($id);
-
-        $tagSync = $this->prepareTagsIds($request->input('tags'));
-
         $product->update($request->all());
-        $product->tags()->sync($tagSync);
+
+        $this->tags->syncProductTags($product, $request->input('tags'));
 
         return redirect()
             ->route('admin.products.index')
@@ -114,25 +109,4 @@ class ProductsController extends Controller
         //
     }
 
-    /**
-     * Return a ids from old and newly created tags.
-     *
-     * @param array $tags List of tags ids and new tags
-     *
-     * @return array
-     */
-    private function prepareTagsIds($tags)
-    {
-        $tagIds = [];
-        foreach ($tags as $key => $tag) {
-            if (!is_numeric($tag)) {
-                $newTag = $this->tags->firstOrCreate(['name' => $tag]);
-                $tagIds[] = $newTag->id;
-            } else {
-                $tagIds[] = $tag;
-            }
-        }
-
-        return $tagIds;
-    }
 }
