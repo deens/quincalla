6015d61b4d01911b05ce821c7b44c20dc4522b51
Denis Ramirez <deensky@gmail.com>
2015-07-24 16:42:36 -0700

    Fix spacing.

--- app/Entities/Order.php
+++ app/Entities/Order.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $table = 'orders';
+
     protected $fillable = [
         'customer_email',
         'customer_name',
