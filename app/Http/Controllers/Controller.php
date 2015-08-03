4d7ee2daf68ca2dc949c9cb3bbb4928080923a1d
Denis Ramirez <deensky@gmail.com>
2015-08-02 21:06:04 -0700

    Add response redirect helpers.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,22 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 abstract class Controller extends BaseController
 {
     use DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Returns a HTTP redirect back with error message
+     */
+    public function redirectBackWithMessage($message)
+    {
+        return redirect()->back()
+            ->with('error', $message);
+    }
+
+    /**
+     * Returns a HTTP redirect back with validation message
+     */
+    public function redirectBackWithValidationErrors($validator)
+    {
+        return redirect()->back()
+            ->withErrors($validator->errors())->withInput();
+    }
 }
